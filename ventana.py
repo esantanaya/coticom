d4eee86a5738834d998b585825313397ae0af7e2
Enrique Santana <esantanaya@gmail.com>
2018-06-06 22:24:32 -0500

    Bosquejo de datos

--- ventana.py
+++ ventana.py
@@ -54,10 +54,8 @@ class PestanaCotizacion(ttk.Frame):
         self.lbl_fecha = tk.Label(self.frame_arriba, text="Fecha")
         self.txt_fecha = tk.Entry(self.frame_arriba, textvariable=self.fecha)
 
-        self.frame_datos = ttk.Frame(self)
         self.frame_detalle = ttk.Frame(self)
 
-        self.lblf_datos = ttk.LabelFrame(self.frame_datos, text="Datos")
         self.lblf_detalle = ttk.LabelFrame(self.frame_detalle, text="Detalle")
 
         self.frame_abajo = tk.Frame(self)
@@ -79,7 +77,6 @@ class PestanaCotizacion(ttk.Frame):
         self.txt_fecha.pack(side=tk.RIGHT)
         self.lbl_fecha.pack(side=tk.RIGHT)
 
-        self.lblf_datos.pack(fill=tk.BOTH, expand=1)
         self.lblf_detalle.pack(fill=tk.BOTH, expand=1)
 
         self.frame_abajo.pack(side=tk.BOTTOM, fill=tk.BOTH)
@@ -87,8 +84,8 @@ class PestanaCotizacion(ttk.Frame):
         self.btn_enviar.pack(side=tk.RIGHT, padx=(50,350))
 
         self.frame_detalle.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
-        self.frame_datos.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
 
+        self.grupo_datos()
         self.grupo_cliente()
 
     def grupo_cliente(self):
@@ -100,6 +97,13 @@ class PestanaCotizacion(ttk.Frame):
 
         self.lblf_cliente.pack(fill=tk.BOTH, expand=1)
         self.frame_cliente.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+
+    def grupo_datos(self):
+        self.frame_datos = ttk.Frame(self)
+        self.lblf_datos = LabelFrameDatos(self.frame_datos, text="Datos")
+
+        self.lblf_datos.pack(fill=tk.BOTH, expand=1)
+        self.frame_datos.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
 
 
 class PestanaCompras(PestanaCotizacion):
@@ -131,8 +135,8 @@ class LabelFrameCliente(ttk.LabelFrame):
         domicilio = tk.Frame(self)
         contacto = tk.Frame(self)
         botones = tk.Frame(self)
-
-        lblf_contacto = ttk.LabelFrame(contacto, text="Contacto")
+        botones_arriba = tk.Frame(botones)
+        botones_abajo = tk.Frame(botones)
 
         self.lbl_clave_cliente = tk.Label(arriba, text="Clave")
         self.cmb_clave_cliente = ttk.Combobox(
@@ -159,11 +163,24 @@ class LabelFrameCliente(ttk.LabelFrame):
             width=125,
         )
 
+        self.lblf_contacto = LabelFrameContacto(contacto, text="Contacto")
+
+        self.btn_nuevo = tk.Button(botones_arriba, text="Nuevo")
+        self.btn_editar = tk.Button(botones_arriba, text="Editar")
+        self.btn_buscar = tk.Button(botones_abajo, text="Buscar")
+        self.btn_guardar = tk.Button(botones_abajo, text="Guardar")
+
         arriba.pack(side=tk.TOP, fill=tk.BOTH)
         domicilio.pack(side=tk.TOP, fill=tk.BOTH)
-        lblf_contacto.pack(fill=tk.BOTH, expand=1)
-        contacto.pack(side=tk.LEFT, fill=tk.BOTH, expand=1, padx=(0,200))
-        botones.pack(side=tk.RIGHT, expand=0)
+        self.lblf_contacto.pack(fill=tk.BOTH, expand=1)
+        contacto.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        botones.pack(side=tk.RIGHT, expand=1, fill=tk.BOTH)
+        botones_arriba.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        botones_abajo.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+        self.btn_nuevo.pack(side=tk.LEFT)
+        self.btn_editar.pack(side=tk.LEFT)
+        self.btn_buscar.pack(side=tk.LEFT)
+        self.btn_guardar.pack(side=tk.LEFT)
         self.lbl_clave_cliente.pack(side=tk.LEFT)
         self.cmb_clave_cliente.pack(side=tk.LEFT)
         self.lbl_nombre_cliente.pack(side=tk.LEFT)
@@ -172,6 +189,128 @@ class LabelFrameCliente(ttk.LabelFrame):
         self.txt_telefonos_cliente.pack(side=tk.LEFT)
         self.lbl_domicilio_cliente.pack(side=tk.LEFT)
         self.txt_domicilio_cliente.pack(side=tk.LEFT)
+
+
+class LabelFrameContacto(ttk.LabelFrame):
+
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.nombre_contacto = tk.StringVar()
+        self.cargo_contacto = tk.StringVar()
+        self.movil_contacto = tk.StringVar()
+        self.correo_contacto = tk.StringVar()
+
+        arriba = tk.Frame(self)
+        abajo = tk.Frame(self)
+
+        self.lbl_nombre_contacto = tk.Label(arriba, text="Nombre")
+        self.txt_nombre_contacto = tk.Entry(
+            arriba,
+            textvariable=self.nombre_contacto
+        )
+        self.lbl_cargo_contacto = tk.Label(arriba, text="Cargo")
+        self.txt_cargo_contacto = tk.Entry(
+            arriba,
+            textvariable=self.cargo_contacto
+        )
+        self.lbl_movil_contacto = tk.Label(abajo, text="Movil")
+        self.txt_movil_contacto = tk.Entry(
+            abajo,
+            textvariable=self.movil_contacto
+        )
+        self.lbl_correo_contacto = tk.Label(abajo, text="Correo")
+        self.txt_correo_contacto = tk.Entry(
+            abajo,
+            textvariable=self.correo_contacto
+        )
+
+        arriba.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        abajo.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+        self.lbl_nombre_contacto.pack(side=tk.LEFT)
+        self.txt_nombre_contacto.pack(side=tk.LEFT)
+        self.lbl_cargo_contacto.pack(side=tk.LEFT)
+        self.txt_cargo_contacto.pack(side=tk.LEFT)
+        self.lbl_movil_contacto.pack(side=tk.LEFT)
+        self.txt_movil_contacto.pack(side=tk.LEFT)
+        self.lbl_correo_contacto.pack(side=tk.LEFT)
+        self.txt_correo_contacto.pack(side=tk.LEFT)
+
+
+class LabelFrameDatos(ttk.LabelFrame):
+
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.condiciones_pago = ("30 días naturales",)
+        self.vigencia = ("7 días",)
+        self.moneda = ("Pesos Mexicanos", "Dolares Americanos",)
+        self.asesor = ("José Luis Arreola",)
+        self.nota_1 = (
+            "FAVOR DE ANOTAR EL NUMERO DE COTIZACION EN SU ORDEN DE COMPRA",
+        )
+        self.nota_2 = ("NO SE ACEPTAN DEVOLUCIONES NI CANCELACIONES",)
+
+        izquierda = tk.Frame(self)
+        iz_arriba = tk.Frame(izquierda)
+        iz_centro = tk.Frame(izquierda)
+        iz_abajo = tk.Frame(izquierda)
+        derecha = tk.Frame(self)
+        der_arriba = tk.Frame(derecha)
+        der_centro = tk.Frame(derecha)
+        der_abajo = tk.Frame(derecha)
+
+        self.lbl_condiciones_pago = tk.Label(iz_arriba, text="Condiciones pago")
+        self.cmb_condiciones_pago = ttk.Combobox(
+            iz_arriba,
+            values=self.condiciones_pago
+        )
+        self.lbl_vigencia = tk.Label(iz_centro, text="Vigencia")
+        self.cmb_vigencia = ttk.Combobox(
+            iz_centro,
+            values=self.vigencia
+        )
+        self.lbl_moneda = tk.Label(iz_abajo, text="Moneda")
+        self.cmb_moneda = ttk.Combobox(
+            iz_abajo,
+            values=self.moneda
+        )
+        self.lbl_asesor = tk.Label(der_arriba, text="Asesor")
+        self.cmb_asesor = ttk.Combobox(
+            der_arriba,
+            values=self.asesor
+        )
+        self.lbl_nota_1 = tk.Label(der_centro, text="Nota 1")
+        self.cmb_nota_1 = ttk.Combobox(
+            der_centro,
+            values=self.nota_1
+        )
+        self.lbl_nota_2 = tk.Label(der_abajo, text="Nota 2")
+        self.cmb_nota_2 = ttk.Combobox(
+            der_abajo,
+            values=self.nota_2
+        )
+
+        izquierda.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        iz_arriba.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        iz_centro.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        iz_abajo.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        derecha.pack(side=tk.RIGHT, fill=tk.BOTH, expand=1)
+        der_arriba.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        der_centro.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        der_abajo.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.lbl_condiciones_pago.pack(side=tk.LEFT)
+        self.cmb_condiciones_pago.pack(side=tk.LEFT)
+        self.lbl_vigencia.pack(side=tk.LEFT)
+        self.cmb_vigencia.pack(side=tk.LEFT)
+        self.lbl_moneda.pack(side=tk.LEFT)
+        self.cmb_moneda.pack(side=tk.LEFT)
+        self.lbl_asesor.pack(side=tk.LEFT)
+        self.cmb_asesor.pack(side=tk.LEFT)
+        self.lbl_nota_1.pack(side=tk.LEFT)
+        self.cmb_nota_1.pack(side=tk.LEFT)
+        self.lbl_nota_2.pack(side=tk.LEFT)
+        self.cmb_nota_2.pack(side=tk.LEFT)
 
 
 if __name__ == "__main__":
