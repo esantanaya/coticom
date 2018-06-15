964384827ab42290cf35b528b5312dfa02f365d7
Enrique Santana <esantanaya@gmail.com>
2018-06-14 21:19:54 -0500

    Corregimos la clase conexion

---
+++ bd.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+
+
+class Conexion:
+    def __init__(self, host='localhost', user='postgres', password='4cP#4j9R92'):
+        self.engine = create_engine('postgresql+psycopg2://' + user + ':' +
+        password + '@localhost:5432/pruebas')
+        self.connection = self.engine.connect()
